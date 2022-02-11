6861b0a70df0ad60de7c4b0417c06791df35cc7b
Ricardo-33 <edermask21@gmail.com>
2022-02-11 17:25:24 -0300

    Referenciando os posts do usuario

--- src/main/java/com/ricardo/workshopmongo/domain/User.java
+++ src/main/java/com/ricardo/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.ricardo.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -10,7 +13,6 @@ import lombok.Data;
 
 @Document(collection = "user")
 @Data
-@AllArgsConstructor
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -18,4 +20,17 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	// DBRef: indica que esse atributo está referenciando uma outra coleção do MongoDB
+	// lazy: garante que os posts sejam carregados apenas se eles forem acessados
+	@DBRef(lazy = true) 
+	private List<Post> posts= new ArrayList<>();
+
+	public User(String id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	
 }
